991fdf1298dbe66c59cb00186b394e3f1ec34ebb
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2024-01-10 01:23:35 +0900

    refacter test

--- Makefile
+++ Makefile
@@ -1,20 +1,27 @@
 CFLAGS=-std=c11 -g -static
-SRCS= main.c tokenize.c parse.c type.c codegen.c file.c
+SRCS= $(wildcard *.c)
 OBJS=$(SRCS:.c=.o)
+
+TEST_SRCS=$(wildcard test/*.c)
+TESTS=$(TEST_SRCS:.c=.exe)
 
 ecc: $(OBJS)
 		$(CC) -o ecc $(OBJS) $(LDFLAGS)
 
 $(OBJS): ecc.h
 
-test: ecc
+test/%.exe: ecc test/%.c
+		$(CC) -o- -E -P -C test/$*.c > test/_.c
+		./ecc test/_.c > test/$*.s
+		$(CC) -static -o $@ test/$*.s -xc test/common
+		rm test/_.c
+
+test: $(TESTS)
 		./test.sh
-		./ecc test/arith.c > tmp.s
-		gcc -xc -c -o common.o test/common
-		gcc -static -o tmp tmp.s common.o
-		./tmp
+		for i in $^; do echo $$i; ./$$i || exit 1; echo; done
 
 clean:
 		rm -f ecc *.o *~ tmp*
+		rm -f test/*.s test/*.o test/*.exe
 
 .PHONY: test clean
--- test/arith.c
+++ test/arith.c
@@ -1,34 +1,35 @@
-int ASSERT();
+#include "test.h"
+
+int assert();
 int printf();
 
 int main() {
-    ASSERT(0, 0, "0");
-    ASSERT(42, 42, "42");
-    ASSERT(21, 5 + 20 - 4, "5+20-4");
-    ASSERT(41, 12 + 34 - 5, "12 + 34 - 5");
-    ASSERT(47, 5 + 6 * 7, "5+6*7");
-    ASSERT(15, 5 * (9 - 6), "5*(9-6)");
-    ASSERT(4, (3 + 5) / 2, "(3+5)/2");
+    ASSERT(0, 0);
+    ASSERT(42, 42);
+    ASSERT(21, 5 + 20 - 4);
+    ASSERT(41, 12 + 34 - 5);
+    ASSERT(47, 5 + 6 * 7);
+    ASSERT(15, 5 * (9 - 6));
+    ASSERT(4, (3 + 5) / 2);
 
-    ASSERT(0, 0 == 1, "0 == 1");
-    ASSERT(1, 42 == 42, "42 == 42");
-    ASSERT(1, 0 != 1, "0 != 1");
-    ASSERT(0, 42 != 42, "42 != 42");
+    ASSERT(0, 0 == 1);
+    ASSERT(1, 42 == 42);
+    ASSERT(1, 0 != 1);
+    ASSERT(0, 42 != 42);
 
-    ASSERT(1, 0 < 1, "0 < 1");
-    ASSERT(0, 1 < 1, "1 < 1");
-    ASSERT(0, 2 < 1, "2 < 1");
-    ASSERT(1, 0 <= 1, "0 <= 1");
-    ASSERT(1, 1 <= 1, "1 <= 1");
-    ASSERT(0, 2 <= 1, "2 <= 1");
+    ASSERT(1, 0 < 1);
+    ASSERT(0, 1 < 1);
+    ASSERT(0, 2 < 1);
+    ASSERT(1, 0 <= 1);
+    ASSERT(1, 1 <= 1);
+    ASSERT(0, 2 <= 1);
 
-    ASSERT(1, 1 > 0, "1 > 0");
-    ASSERT(0, 1 > 1, "1 > 1");
-    ASSERT(0, 1 > 2, "1 > 2");
-    ASSERT(1, 1 >= 0, "1 >= 0");
-    ASSERT(1, 1 >= 1, "1 >= 1");
-    ASSERT(0, 1 >= 2, "1 >= 2");
+    ASSERT(1, 1 > 0);
+    ASSERT(0, 1 > 1);
+    ASSERT(0, 1 > 2);
+    ASSERT(1, 1 >= 0);
+    ASSERT(1, 1 >= 1);
+    ASSERT(0, 1 >= 2);
 
-    printf("OK\n");
     return 0;
 }
